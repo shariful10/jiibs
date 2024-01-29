5aab9384da3910f066fa14021407c6ae26ac7d49
Ahmed Nashif <a.nashif7@gmail.com>
2024-01-29 20:22:49 +0600

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavigationBar from "@/Components/Shared/NavigationBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} max-w-[1380px] m-auto`}>
+        <NavigationBar></NavigationBar>
+        {children}
+      
+      </body>
     </html>
   );
 }
