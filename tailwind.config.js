c8ea673cbddaeb8969ae62b4c801386fcc0bbbee
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-01-30 14:45:10 +0600

    Profile page tab section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       fontFamily: {},
       colors: {
         primary: "#2AB7DD",
+        softGray: "#E4E4E4",
+        darkGray: "#626262",
       },
       boxShadow: {
         "3xl": "0 23px 37px 0 #1FBAFF10",
