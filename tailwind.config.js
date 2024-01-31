274c5e2a4cb714f4052cff1978b277ec5f149833
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-01-31 23:05:50 +0600

    git push after pull

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   mode: "jit",
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app//*.{js,ts,jsx,tsx,mdx}",
+    "./pages//*.{js,ts,jsx,tsx,mdx}",
+    "./components//*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using src directory:
+    "./src//*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
