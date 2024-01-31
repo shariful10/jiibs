d3ca817c11092c7e23f902c2cbcbcfeb85bc20cb
Ahmed Nashif <a.nashif7@gmail.com>
2024-01-31 09:14:21 +0600

    none

--- src/app/building-details/page.js
+++
@@ -1,9 +0,0 @@
-import BuildingDetails from "@/Components/Pages/building-details/BuildingDetails";
-
-export default function Home() {
-  return (
-    <>
-      <BuildingDetails />
-    </>
-  );
-}
