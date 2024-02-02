9b64217a41877f2473f41dfec91ea823455978ad
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-02-03 03:07:54 +0600

    Refector: Personal Info Page

--- src/app/(root)/account/personal-info/page.jsx
+++ src/app/(root)/account/personal-info/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import leftIcon from "@/assets/left-icon.svg"
+import leftIcon from "@/assets/left-icon.svg";
 import Container from "@/Components/Regular/Container";
 import PageTitle from "@/Components/Pages/profileInfo/PageTitle";
 import ProfileInfoDetails from "@/Components/Pages/profileInfo/ProfileInfoDetails";
@@ -13,31 +13,33 @@ const PersonalInfo = () => {
 	const [editEmail, setEditEmail] = useState(false);
 	const [editPhone, setEditPhone] = useState(false);
 
-   const handleClose = () => {
-      if (rename) setRename(false);
-      if (editPass) setEditPass(false);
-      if (editEmail) setEditEmail(false);
-      if (editPhone) setEditPhone(false);
-    };
+	const handleClose = () => {
+		if (rename) setRename(false);
+		if (editPass) setEditPass(false);
+		if (editEmail) setEditEmail(false);
+		if (editPhone) setEditPhone(false);
+	};
 
 	return (
 		<Container>
 			<div className="mt-10 mb-20">
 				<div className="md:hidden">
-               {rename || editPass || editEmail || editPhone ? (
-                  <div className="flex items-center">
-                     <Link href="/account/personal-info" onClick={handleClose}>
-                        <Image src={leftIcon} alt="leftIcon" />
-                     </Link>
-                     <p className="w-[80%] text-center">Editing Personal Info</p>
-                  </div>
-               ) : (
-                  <PageTitle />
-               )}
-            </div>
-            <div className="hidden md:block">
-               <PageTitle />
-            </div>
+					{rename || editPass || editEmail || editPhone ? (
+						<div className="flex items-center">
+							<Link href="/account/personal-info" onClick={handleClose}>
+								<Image src={leftIcon} alt="leftIcon" />
+							</Link>
+							<p className="w-[80%] text-center">
+								Editing Personal Info
+							</p>
+						</div>
+					) : (
+						<PageTitle />
+					)}
+				</div>
+				<div className="hidden md:block">
+					<PageTitle />
+				</div>
 				<ProfileInfoDetails
 					rename={rename}
 					editPass={editPass}
