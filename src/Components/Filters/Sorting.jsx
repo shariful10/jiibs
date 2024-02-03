09f8ef9b71da622f20152494af28afe542dbe9b4
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-03 13:41:38 +0600

    fixed sorting issues

--- src/Components/Filters/Sorting.jsx
+++ src/Components/Filters/Sorting.jsx
@@ -21,20 +21,19 @@ export default function Sorting() {
     },
   ]);
 
-  const handleChecked = (option) => {
-    const index = sorting.indexOf(option);
-    console.log(index);
-    if (index !== -1 && index?.isFiltered) {
-      const updatedFiltering = sorting[option?.isFiltered];
-      console.log(updatedFiltering);
-      //   setSorting(updatedFiltering);
-    }
+  const handleChecked = (index) => {
+    const updatedSorting = [...sorting];
+    updatedSorting[index].isFiltered = !updatedSorting[index].isFiltered;
+    setSorting(updatedSorting);
   };
 
   return (
     <div className="flex flex-col gap-6 py-3">
       {sorting?.map((option, index) => (
-        <div className="flex items-center justify-between py-0 px-4">
+        <div
+          key={index}
+          className="flex items-center justify-between py-0 px-4"
+        >
           <span>{option?.label}</span>
           <Checkbox
             isChecked={option?.isFiltered}
