d3fd65b2531d7774b0a4195d060b74d07a639d0a
Shipon Hossen Raju <msshipon234@gmail.com>
2024-02-03 23:00:18 +0600

    responsive complete

--- src/Components/Pages/building-details/Description/Description.jsx
+++ src/Components/Pages/building-details/Description/Description.jsx
@@ -4,14 +4,14 @@ import LocationMap from "./LocationDetails/LocationMap";
 export default function Description() {
   return (
     <>
-      <div className="grid grid-cols-1 mdx:grid-cols-12 gap-20">
+      <div className="flex flex-col-reverse md:flex-row flex-start items-start justify-between gap-10 md:gap-20 w-full">
         {/* about , description */}
-        <div className="mdx:col-span-8">
+        <div className="w-full md:w-4/6">
           <AboutSide />
         </div>
 
         {/* Location map */}
-        <div className="mdx:col-span-4 order-1 mdx:order-2">
+        <div className="w-full md:w-2/6">
           <LocationMap />
         </div>
       </div>
