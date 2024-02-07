cf50ac360d93a70234e4fc3d71f6a7229b2e6666
rakib53 <73785018+rakib53@users.noreply.github.com>
2024-02-07 13:43:29 +0600

    added context api

---
+++ src/Context/Context.js
@@ -0,0 +1,5 @@
+"use client";
+const { createContext, useState } = require("react");
+const Context = createContext();
+
+export default Context;
--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Footer from "@/Components/Shared/Footer";
 import NavigationBar from "@/Components/Shared/NavigationBar";
+import Context from "@/Context/Context";
 
 const Layout = ({ children }) => {
-	return (
-		<div>
-			<NavigationBar />
-         {children}
-			<Footer />
-		</div>
-	);
+  const [value, setvalue] = useState({
+    modal: {
+      isModalOpen: false,
+      modalType: "",
+      isMobileModal: false,
+      modalLabel: "",
+      isWidth: "",
+    },
+  });
+
+  return (
+    <div>
+      <Context.Provider value={(value, setvalue)}>
+        <NavigationBar />
+        {children}
+        <Footer />
+      </Context.Provider>
+    </div>
+  );
 };
 
 export default Layout;
