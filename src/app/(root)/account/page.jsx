af02db33e7ef058e6f6948cae8b112b742a98582
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-02-09 00:54:14 +0600

    refactor: categories

--- src/app/(root)/account/page.jsx
+++ src/app/(root)/account/page.jsx
@@ -3,25 +3,24 @@ import Container from "@/Components/Regular/Container";
 import Link from "next/link";
 import React from "react";
 
-
 const AccountPage = () => {
-	return (
-		<Container>
-			<div className="mt-10 mb-20">
-				<h2 className="text-[36px] font-semibold">Account</h2>
-				<div className="flex items-end gap-4 mb-10 text-darkGray">
-					<h4 className="text-2xl font-semibold ">Jane smith</h4>
-					<Link
-						href="#"
-						className="text-lg underline hover:text-primary duration-300 cursor-pointer"
-					>
-						Go to profile
-					</Link>
-				</div>
-            <AccountPageCard />
-			</div>
-		</Container>
-	);
+  return (
+    <Container>
+      <div className="mt-10 mb-20 p-5 md:p-0">
+        <h2 className="text-[36px] font-semibold">Account</h2>
+        <div className="flex items-end gap-4 mb-10 text-darkGray">
+          <h4 className="text-2xl font-semibold ">Jane smith</h4>
+          <Link
+            href="#"
+            className="text-lg underline hover:text-primary duration-300 cursor-pointer"
+          >
+            Go to profile
+          </Link>
+        </div>
+        <AccountPageCard />
+      </div>
+    </Container>
+  );
 };
 
 export default AccountPage;
--- src/app/(root)/tours-and-open-houses/page.jsx
+++ src/app/(root)/tours-and-open-houses/page.jsx
@@ -5,53 +5,53 @@ import Container from "@/Components/Regular/Container";
 import UpcomingTab from "@/Components/Pages/ToursAndOpenHouses/UpcomingTab";
 
 const ToursAndOpenHouses = () => {
-	const [tab, setTab] = useState("upcoming");
+  const [tab, setTab] = useState("upcoming");
 
-	return (
-		<Container>
-			<div className="my-10">
-				<div className="flex justify-between items-center">
-					<div className="text-darkGray flex items-center gap-6">
-						<p
-							onClick={() => setTab("upcoming")}
-							className={`${
-								tab === "upcoming"
-									? "text-primary border-b-2 border-primary"
-									: "border-b-2 border-transparent"
-							} pb-2 cursor-pointer hover:text-primary`}
-						>
-							Upcoming
-						</p>
-						<p
-							onClick={() => setTab("past")}
-							className={`${
-								tab === "past"
-									? "text-primary border-b-2 border-primary"
-									: "border-b-2 border-transparent"
-							} pb-2 cursor-pointer hover:text-primary`}
-						>
-							Past
-						</p>
-					</div>
-					<Link href="#">
-						<button className="text-white bg-primary py-2 px-4 rounded-md">
-							Explore
-						</button>
-					</Link>
-				</div>
-				{tab === "upcoming" && (
-					<div className="mt-10">
-                  <UpcomingTab />
-               </div>
-				)}
-				{tab === "past" && (
-					<div className="mt-10">
-                  <h2 className="text-2xl font-semibold text-center">Coming soon</h2>
-               </div>
-				)}
-			</div>
-		</Container>
-	);
+  return (
+    <Container>
+      <div className="my-10 p-5 md:p-0">
+        <div className="flex justify-between items-center">
+          <div className="text-darkGray flex items-center gap-6">
+            <p
+              onClick={() => setTab("upcoming")}
+              className={`${
+                tab === "upcoming"
+                  ? "text-primary border-b-2 border-primary"
+                  : "border-b-2 border-transparent"
+              } pb-2 cursor-pointer hover:text-primary`}
+            >
+              Upcoming
+            </p>
+            <p
+              onClick={() => setTab("past")}
+              className={`${
+                tab === "past"
+                  ? "text-primary border-b-2 border-primary"
+                  : "border-b-2 border-transparent"
+              } pb-2 cursor-pointer hover:text-primary`}
+            >
+              Past
+            </p>
+          </div>
+          <Link href="#">
+            <button className="text-white bg-primary py-2 px-4 rounded-md">
+              Explore
+            </button>
+          </Link>
+        </div>
+        {tab === "upcoming" && (
+          <div className="mt-10">
+            <UpcomingTab />
+          </div>
+        )}
+        {tab === "past" && (
+          <div className="mt-10">
+            <h2 className="text-2xl font-semibold text-center">Coming soon</h2>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default ToursAndOpenHouses;
