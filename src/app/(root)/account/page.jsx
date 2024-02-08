220d37c0ced222fd1cd57adea438f6f1dceced6f
Sk Shariful Islam <freelancershariful10@gmail.com>
2024-02-09 03:10:38 +0600

    Refector: Account Page, Personal Info Page, Tours and Open Houses Page & Footer

--- src/app/(root)/account/page.jsx
+++ src/app/(root)/account/page.jsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 import React from "react";
 
 const AccountPage = () => {
-  return (
-    <Container>
-      <div className="mt-10 mb-20 p-5 md:p-0">
-        <h2 className="text-[36px] font-semibold">Account</h2>
-        <div className="flex items-end gap-4 mb-10 text-darkGray">
-          <h4 className="text-2xl font-semibold ">Jane smith</h4>
-          <Link
-            href="#"
-            className="text-lg underline hover:text-primary duration-300 cursor-pointer"
-          >
-            Go to profile
-          </Link>
-        </div>
-        <AccountPageCard />
-      </div>
-    </Container>
-  );
+	return (
+		<Container className="px-6 md:px-0">
+			<div className="mt-10 mb-20">
+				<h2 className="text-[36px] font-semibold">Account</h2>
+				<div className="flex items-end gap-4 mb-10 text-darkGray">
+					<h4 className="text-2xl font-semibold ">Jane smith</h4>
+					<Link
+						href="#"
+						className="text-lg underline hover:text-primary duration-300 cursor-pointer"
+					>
+						Go to profile
+					</Link>
+				</div>
+				<AccountPageCard />
+			</div>
+		</Container>
+	);
 };
 
 export default AccountPage;
