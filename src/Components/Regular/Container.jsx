f82d3e20e8021f0299866174613d2bb4bc2c6ed9
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-02-09 01:49:34 +0600

    refactor: container responsive

--- src/Components/Regular/Container.jsx
+++ src/Components/Regular/Container.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Container({ children, className }) {
   return (
     <div
-      className={`w-full max-[1128px]:max-w-[1024px] max-[1368px]:max-w-[1120px] xl:max-w-[1280px] max-[1920px]:max-w-[1520px] mx-auto px-6 md:px-0 ${
+      className={`w-full max-[1128px]:max-w-[1024px] max-[1368px]:max-w-[1120px] xl:max-w-[1280px] max-[1920px]:max-w-[1520px] mx-auto ${
         className ? className : null
       }`}
     >
