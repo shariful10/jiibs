f48f0163e166063e6fe305479cb356cb701fa981
rakib53 <73785018+rakib53@users.noreply.github.com>
2024-02-08 12:48:17 +0600

    Added new fetures in modal

--- src/Components/wishlist/AddWishlistCard.jsx
+++ src/Components/wishlist/AddWishlistCard.jsx
@@ -4,18 +4,17 @@ import Image from "next/image";
 
 export default function AddWishlistCard() {
   return (
-    // <div
-    //   style={{
-    //     boxShadow: "0px 0px 17px 4px #B1B1B140",
-    //   }}
-    //   className="max-w-[325px] w-full h-full p-1 rounded-2xl"
-    // >
-
-    // </div>
-    <Image
-      src={midtown}
-      className="w-[325px] h-[275px] object-cover rounded-2xl custom-scrollbar"
-      alt="image"
-    />
+    <span className="block w-[325px] h-[250px]">
+      <Image
+        src={midtown}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          borderRadius: "8px",
+        }}
+        alt="image"
+      />
+    </span>
   );
 }
