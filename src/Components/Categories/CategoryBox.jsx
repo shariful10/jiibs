d52245dffde6f5354e39a4feda89381c004526eb
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-08 22:35:46 +0600

    fixed issue in desktop header category

--- src/Components/Categories/CategoryBox.jsx
+++ src/Components/Categories/CategoryBox.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 const CategoryBox = ({ label, icon }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer">
+    <div className="flex flex-col items-center justify-center gap-2 border-b-2 hover:text-neutral-800 border-transparent text-neutral-500 cursor-pointer">
       <Image width={28} src={icon} alt={label} />
-      <div className="text-sm font-medium">{label}</div>
+      <div className="text-base font-normal leading-6">{label}</div>
     </div>
   );
 };
