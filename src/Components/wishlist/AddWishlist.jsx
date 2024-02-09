68e1636fb6b5eee336675f0c40bbc9661fd294f4
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-09 23:39:07 +0600

    Fixed issue in add wishlist

--- src/Components/wishlist/AddWishlist.jsx
+++ src/Components/wishlist/AddWishlist.jsx
@@ -1,46 +1,13 @@
 import React from "react";
-import midtown from "@/assets/images/apartment/Midtown-East.png";
-import Image from "next/image";
+import AddWishlistCard from "./AddWishlistCard";
 
 export default function AddWishlist() {
   return (
     <div className="w-full p-6 overflow-hidden">
-      <div className="max-w-[750px] w-full">
-        <div className="flex flex-nowrap gap-6 overflow-x-auto rounded-2xl pb-5 custom-scrollbar">
-          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
-            <Image
-              src={midtown}
-              alt="image"
-              width="325px"
-              height="250px"
-              className="w-full"
-            />
-            <h4>sdsd</h4>
-            <p>sdsd</p>
-          </div>
-          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
-            <Image
-              src={midtown}
-              alt="image"
-              width="325px"
-              height="250px"
-              className="w-full"
-            />
-            <h4>sdsd</h4>
-            <p>sdsd</p>
-          </div>
-          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
-            <Image
-              src={midtown}
-              alt="image"
-              width="325px"
-              height="250px"
-              className="w-full"
-            />
-            <h4>sdsd</h4>
-            <p>sdsd</p>
-          </div>
-        </div>
+      <div className="w-[750px] flex gap-6 overflow-x-auto rounded-2xl pb-5 custom-scrollbar p-3">
+        <AddWishlistCard />
+        <AddWishlistCard />
+        <AddWishlistCard />
       </div>
       <div className="flex justify-center w-full mt-5">
         <button className="w-full font-semibold text-base leading-5 bg-primary py-4 px-8 outline-none border-none rounded-md text-white text-center">
