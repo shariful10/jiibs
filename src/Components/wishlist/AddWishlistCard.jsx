be8e2ab56d5240c75b18aedcabdbde3139eae6ee
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-09 13:09:31 +0600

    fixed add wishlist scroll bar color

--- src/Components/wishlist/AddWishlistCard.jsx
+++ src/Components/wishlist/AddWishlistCard.jsx
@@ -4,17 +4,27 @@ import Image from "next/image";
 
 export default function AddWishlistCard() {
   return (
-    <span className="block w-[325px] h-[250px]">
-      <Image
-        src={midtown}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          borderRadius: "8px",
-        }}
-        alt="image"
-      />
-    </span>
+    // <span className="block w-[325px] h-[250px]">
+    //   <Image
+    //     src={midtown}
+    //     style={{
+    //       width: "100%",
+    //       height: "100%",
+    //       objectFit: "cover",
+    //       borderRadius: "8px",
+    //     }}
+    //     alt="image"
+    //   />
+    // </span>
+    <Image
+      src={midtown}
+      style={{
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+        borderRadius: "8px",
+      }}
+      alt="image"
+    />
   );
 }
