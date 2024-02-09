47c19c51e81a23ce2aaf2da256db0c8a884225b4
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-02-09 23:10:46 +0600

    refactor: wishlist scrolling updated

--- src/Components/wishlist/AddWishlist.jsx
+++ src/Components/wishlist/AddWishlist.jsx
@@ -5,43 +5,41 @@ import Image from "next/image";
 export default function AddWishlist() {
   return (
     <div className="w-full p-6 overflow-hidden">
-      <div className="w-[750px] grid grid-cols-2 gap-6 overflow-x-auto rounded-2xl">
-        <div className="w-[325px] bg-slate-400 p-2 rounded-lg">
-          <Image
-            src={midtown}
-            alt="image"
-            width="100%"
-            height="250px"
-            objectFit="contain"
-            layout="responsive"
-            className="w-full"
-          />
-          <h4>sdsd</h4>
-          <p>sdsd</p>
-        </div>
-        <div className="w-[325px] bg-slate-200 p-2 rounded-lg">
-          <Image
-            src={midtown}
-            alt="image"
-            width="325px"
-            height="250px"
-            objectFit="contain"
-            layout="responsive"
-          />
-          <h4>sdsd</h4>
-          <p>sdsd</p>
-        </div>
-        <div className="w-[325px] bg-slate-200 p-2 rounded-lg">
-          <Image
-            src={midtown}
-            alt="image"
-            width="325px"
-            height="250px"
-            objectFit="contain"
-            layout="responsive"
-          />
-          <h4>sdsd</h4>
-          <p>sdsd</p>
+      <div className="max-w-[750px] w-full">
+        <div className="flex flex-nowrap gap-6 overflow-x-auto rounded-2xl pb-5 custom-scrollbar">
+          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
+            <Image
+              src={midtown}
+              alt="image"
+              width="325px"
+              height="250px"
+              className="w-full"
+            />
+            <h4>sdsd</h4>
+            <p>sdsd</p>
+          </div>
+          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
+            <Image
+              src={midtown}
+              alt="image"
+              width="325px"
+              height="250px"
+              className="w-full"
+            />
+            <h4>sdsd</h4>
+            <p>sdsd</p>
+          </div>
+          <div className=" bg-slate-200 p-2 rounded-lg min-w-[325px]">
+            <Image
+              src={midtown}
+              alt="image"
+              width="325px"
+              height="250px"
+              className="w-full"
+            />
+            <h4>sdsd</h4>
+            <p>sdsd</p>
+          </div>
         </div>
       </div>
       <div className="flex justify-center w-full mt-5">
