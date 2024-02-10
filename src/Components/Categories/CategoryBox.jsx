76fba0734fd7a13a07a6dda5de8c9df01b23ce81
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-10 10:07:13 +0600

    changes

--- src/Components/Categories/CategoryBox.jsx
+++ src/Components/Categories/CategoryBox.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CategoryBox = ({ label, icon }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 border-b-2 border-transparent cursor-pointer">
+    <div className="flex flex-col items-center justify-center gap-2 border-b-2 border-transparent cursor-pointer focus:border-black">
       <Image width={28} src={icon} alt={label} />
       <div className="text-base font-normal leading-6 text-blackText">
         {label}
