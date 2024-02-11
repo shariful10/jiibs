d5664cab72658aac6fe1cdc04fcb36a9f57b4d6c
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-11 23:42:26 +0600

    Added sticky while scrolling building details page

--- src/Components/Pages/building-details/BuildingDetails.jsx
+++ src/Components/Pages/building-details/BuildingDetails.jsx
@@ -6,43 +6,39 @@ import Similar from "./Similar/Similar";
 import BuildingSlider from "@/Components/BuildingImageSlider/Slider";
 
 export default function BuildingDetail() {
-	return (
-		<>
-			<div className="relative">
-				{/* title and slider part */}
-				<Container className="py-4">
-					{/* Header part */}
-					<div className="mdx:flex flex-col mdx:flex-row gap-4 justify-between items-center py-[18px] hidden">
-						<h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">
-							70 Pine Street, New York, New york 10005
-						</h1>
+  return (
+    <>
+      <div className="relative">
+        {/* title and slider part */}
+        <section className="py-4">
+          {/* Header part */}
+          <div className="mdx:flex flex-col mdx:flex-row gap-4 justify-between items-center py-[18px] hidden">
+            <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold">
+              70 Pine Street, New York, New york 10005
+            </h1>
 
-						<div className="hidden md:block">
-							<Breadcrumbs
-								paths={[
-									{ name: "buildings", link: "#" },
-									{ name: "Downtown", link: "#" },
-									{ name: "Financial district", link: "#" },
-									{ name: "70 pine street", link: "#" },
-								]}
-							/>
-						</div>
-					</div>
+            <div className="hidden md:block">
+              <Breadcrumbs
+                paths={[
+                  { name: "buildings", link: "#" },
+                  { name: "Downtown", link: "#" },
+                  { name: "Financial district", link: "#" },
+                  { name: "70 pine street", link: "#" },
+                ]}
+              />
+            </div>
+          </div>
 
-					{/* preview slider */}
-					<BuildingSlider />
-				</Container>
+          {/* preview slider */}
+          <BuildingSlider />
+        </section>
 
-				{/* Description */}
-				<Container>
-					<Description />
-				</Container>
+        {/* Description */}
+        <Description />
 
-				{/* Similar Buildings */}
-				<Container>
-					<Similar />
-				</Container>
-			</div>
-		</>
-	);
+        {/* Similar Buildings */}
+        <Similar />
+      </div>
+    </>
+  );
 }
