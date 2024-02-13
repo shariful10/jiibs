d70487de75b8ebf856523e32d532130e80bd152e
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-14 00:08:08 +0600

    Fixed container width issues

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -9,27 +9,27 @@ import Container from "@/Components/Regular/Container";
 import Footer from "@/Components/Shared/Footer";
 
 const Layout = ({ children }) => {
-	const [modal, setModal] = useState({
-		isOpen: false,
-		modalType: "",
-		isMobileModal: false,
-		modalLabel: "",
-		isWidth: "",
-		topRightContent: "",
-	});
+  const [modal, setModal] = useState({
+    isOpen: false,
+    modalType: "",
+    isMobileModal: false,
+    modalLabel: "",
+    isWidth: "",
+    topRightContent: "",
+  });
 
-	return (
-		<Context.Provider value={{ modal, setModal }}>
-			<Container>
-				<NavigationBar />
-				<MobileBottomMenu />
-				{children}
-			</Container>
-			<Footer />
-			{/* Modal  */}
-			{modal?.isOpen && <Modal {...modal} />}
-		</Context.Provider>
-	);
+  return (
+    <Context.Provider value={{ modal, setModal }}>
+      <Container>
+        <NavigationBar />
+        <MobileBottomMenu />
+        {children}
+      </Container>
+      <Footer />
+      {/* Modal  */}
+      {modal?.isOpen && <Modal {...modal} />}
+    </Context.Provider>
+  );
 };
 
 export default Layout;
