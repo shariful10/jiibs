10cb3c1dea55fc272a14ba9340de024a91233c57
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-02-13 16:22:36 +0600

    categories section done

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -4,31 +4,28 @@ import { categories } from "./categoriesData";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./Categories.css";
 
-import { FreeMode, Pagination } from "swiper/modules";
+import { FreeMode, Navigation } from "swiper/modules";
 
 const Categories = () => {
-	return (
-		<div>
-			<Swiper
-				freeMode={true}
-				// pagination={{
-				//   clickable: true,
-				// }}
-				modules={[FreeMode, Pagination]}
-				className="mySwiper md:max-w-[380px] mdx:max-w-[630px] lg:max-w-[720px] elg:max-w-[960px]"
-			>
-				{categories.map((category) => (
-					<SwiperSlide key={category.label}>
-						<CategoryBox label={category.label} icon={category.icon} />
-					</SwiperSlide>
-				))}
-			</Swiper>
-		</div>
-	);
+  return (
+    <div>
+      <Swiper
+        slidesPerView={7}
+        // navigation={true}
+        modules={[FreeMode, Navigation]}
+        className="mySwiper w-full md:max-w-[380px] mdx:max-w-[630px] lg:max-w-[720px] elg:max-w-[960px]"
+      >
+        {categories.map((category) => (
+          <SwiperSlide key={category.label}>
+            <CategoryBox label={category.label} icon={category.icon} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Categories;
