257beb3f30eace35d5421817c631a33feebfc42f
Rakib Hossen <73785018+rakib53@users.noreply.github.com>
2024-02-17 00:32:25 +0600

    changes logo name

--- src/Components/Shared/Logo.jsx
+++ src/Components/Shared/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <>
       <div className="flex text-[32px] leading-[47px] font-semibold text-primary font-roboto">
-        <Link href="/">JIBBS.</Link>
+        <Link href="/">JIIBS.</Link>
       </div>
     </>
   );
